fdeed4050219c46895ea326324a903e77a331067
Keila <keilasol930@gmail.com>
2024-06-18 15:47:21 -0300

    Subiendo repo

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+import colores
+
+pygame.init() 
+
+window = pygame.display.set_mode((1200,800)) 
+
+#establezco titulo de la venta:
+pygame.display.set_caption("Adivina el logo")
+
+# pygame carga una imagen,el load representa una superficie
+icon = pygame.image.load("imagenes\icono.png")
+#seteo el icono:
+pygame.display.set_icon(icon)
+
+#rellena la ventana de un color:
+window.fill(colores.LIGHT_BROWN)
+
+flag = True
+while flag:
+    list_event = pygame.event.get()
+    for event in list_event:
+        if event.type == pygame.QUIT: #PREGUNTO SI PRESIONO la x de la ventana para salir
+            flag = False
+    #Todo los obejtos superficies que meta en la ventana lo tengo que actualizar con el update
+    pygame.display.update() #hasta que no actualizo la ventana no la cambio de estado entonces el color no se pone
+
+pygame.quit()
